c71b3edc85084d38e308473a1afc5f97c3f8c6b4
FengFan <me@fengfan.me>
2017-09-27 02:28:06 +0800

    auto deploy

--- shipitfile.js
+++ shipitfile.js
@@ -12,7 +12,7 @@ module.exports = function (shipit) {
       keepReleases: 5,
       key: '~/.ssh/id_rsa',
       shallowClone: true,
-      servers: 'root@45.76.207.106'
+      servers: 'root@97.64.19.213'
     }
   })
 
@@ -42,6 +42,6 @@ module.exports = function (shipit) {
   })
 
   shipit.blTask('run', function() {
-    return shipit.remote(`docker run -e VIRTUAL_HOST=api.oct16.cn --name blog_be -v /home/go_files:/go/src -d -p 3016:3016 golang /bin/bash -c "cd /go/src/echo-blog && ./gorun.sh"`)
+    return shipit.remote(`docker run -e --name blog_be -v /home/go_files:/go/src -d -p 3016:3016 golang /bin/bash -c "cd /go/src/echo-blog && ./gorun.sh"`)
   })
 }
